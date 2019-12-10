65b8a9e6f8cca0fb4ac61a509d5a4f8f05e1077b
Cam <c4miloei@gmail.com>
2019-12-09 23:16:58 -0800

    Add final resources, lambda and APL docs

--- index.js
+++ index.js
@@ -3,45 +3,28 @@
 // session persistence, api calls, and more.
 const Alexa = require("ask-sdk-core");
 const launchDocument = require("documents/launchDocument.json");
+const playDocument = require("documents/playDocument.json");
+const screenDocument = require("documents/screenDocument.json");
 const util = require("./util");
 
-const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
-    );
-  },
-  handle(handlerInput) {
-    if (
-      Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)[
-        "Alexa.Presentation.APL"
-      ]
-    ) {
-      handlerInput.responseBuilder.addDirective({
-        type: "Alexa.Presentation.APL.RenderDocument",
-        document: launchDocument,
-        datasources: {
-          text: {
-            type: "object",
-            welcome: "Welcome",
-            middle: "to",
-            end: "creepy maze!"
-          },
-          assets: {
-            wakingUp: util.getS3PreSignedUrl("Media/1.png")
-          }
-        }
-      });
-    }
-
-    const speakOutput =
-      "Welcome to creepy maze! . To play creepy maze you can say play or if you need help say help";
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt(speakOutput)
-      .getResponse();
+function renderDocumentForScreen(document, handlerInput, text, assets) {
+  if (
+    Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)[
+      "Alexa.Presentation.APL"
+    ]
+  ) {
+    handlerInput.responseBuilder.addDirective({
+      type: "Alexa.Presentation.APL.RenderDocument",
+      document,
+      datasources: {
+        text,
+        assets
+      }
+    });
+  } else {
+    console.error("Device does not support APL");
   }
-};
+}
 
 function buildNotInitializedResponse(handlerInput) {
   const speakOutput = "You are still spleeping, say play to wake up.";
@@ -83,6 +66,7 @@ function handleMovement(direction) {
     nextPosition.y += 1;
   }
 
+  // == null to avoid checking undefined too
   if (
     maze[nextPosition.x] == null ||
     maze[nextPosition.x][nextPosition.y] == null
@@ -92,49 +76,55 @@ function handleMovement(direction) {
 
   const tile = maze[nextPosition.x][nextPosition.y];
 
-  const isHit = Math.ceil(Math.random() * 101) - 1 > 50;
-  let isBonfire = false;
   let isExit = false;
-  let die = false;
 
   let speakOutput = "";
+  let imageSrc = "";
+  let isHit = false;
 
   switch (tile) {
     case "bat":
+      isHit = Math.ceil(Math.random() * 101) - 1 > 50;
       speakOutput = isHit
         ? "You found a bat an immediately bites you! . you lost 1 point of health."
         : "a bat stares at you but seems like is distracted with a different prey. ";
+      imageSrc = "bat.svg";
+      player.life = isHit ? player.life - 1 : player.life;
       break;
     case "wolf":
+      isHit = Math.ceil(Math.random() * 101) - 1 > 50;
       speakOutput = isHit
         ? "A wolf starts chasing you and ends up doing serious damage . you lost 1 point of health."
         : "You found a wolf, but he's ignoring you because you seem too weak. ";
+      imageSrc = "wolf.svg";
+      player.life = isHit ? player.life - 1 : player.life;
       break;
     case "empty":
       speakOutput =
         "There's nothing here, only trees and the moon in the sky. ";
+      imageSrc = "empty.svg";
       break;
     case "bonfire":
+      player.life = player.life + 1;
       speakOutput =
         "You found an abandoned bonfire, part of your health is restored. You recovered 1 point of health. ";
-      isBonfire = true;
+      imageSrc = "bonfire.svg";
       break;
     case "exit":
       speakOutput = `Hooorrayy, you finally found the exit. After leaving the forest you promise yourself that you will never come back, so you burn it. 
-                Hours after, in your plane back to home, you realize it starts falling down into a different forest. Before the crash everything fades away again.`;
+                Hours after, in your plane back to home, you realize it starts falling down into a different forest. Before the crash everything fades away again.
+                 The End?`;
       isExit = true;
+      imageSrc = "exit.svg";
       break;
   }
 
   let dieText = "";
-
-  player.life = isHit ? player.life - 1 : player.life;
-  player.life = isBonfire ? player.life + 1 : player.life;
   player.position = nextPosition;
 
   if (player.life <= 0) {
     dieText =
-      "After being hit you lie in the ground without energy. Seconds after everything fades away.";
+      " After being hit you lie in the ground without energy. Seconds after everything fades away. ";
     reset();
   }
 
@@ -144,7 +134,8 @@ function handleMovement(direction) {
 
   return {
     speakOutput,
-    dieText
+    dieText,
+    imageSrc
   };
 }
 
@@ -193,6 +184,35 @@ let player = {
   life: 5
 };
 
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+    const text = {
+      type: "object",
+      welcome: "Welcome",
+      middle: "to",
+      end: "creepy maze!"
+    };
+
+    const assets = {
+      forest: util.getS3PreSignedUrl("Media/forest.svg")
+    };
+
+    renderDocumentForScreen(launchDocument, handlerInput, text, assets);
+
+    const speakOutput =
+      "Welcome to creepy maze! . To play creepy maze you can say play or if you need help say help";
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  }
+};
+
 const PlayIntentHandler = {
   canHandle(handlerInput) {
     return (
@@ -209,10 +229,19 @@ const PlayIntentHandler = {
     reset();
     initialized = true;
 
-    console.log("PLAY INTENT", "maze", maze, "player", player);
+    const text = {
+      type: "object",
+      main:
+        "You woke up at night, in the middle of the forest. You can tell by the trees that surround you. Try to stand up."
+    };
 
-    const speakOutput =
-      "You woke up at night, in the middle of the forest. You can tell by the trees that surround you. Try to stand up.";
+    const assets = {
+      wakingUp: util.getS3PreSignedUrl("Media/1.png")
+    };
+
+    renderDocumentForScreen(playDocument, handlerInput, text, assets);
+
+    const speakOutput = text.main;
     return handlerInput.responseBuilder
       .speak(speakReset + speakOutput)
       .reprompt(speakReset + speakOutput)
@@ -235,15 +264,25 @@ const StandUpIntentHandler = {
     let extraText = "";
 
     if (standUp) {
-      extraText = "You are already stand up.";
+      extraText = "You are already stand up. But ";
     }
 
     standUp = true;
 
-    console.log("STAND UP INTENT", "maze", maze, "player", player);
+    const text = {
+      type: "object",
+      main:
+        "you only see trees and four different directions. Move by saying go north, go south, go west or go east, to escape from this creepy forest. ",
+      life: "-1"
+    };
 
-    const speakOutput =
-      "You only see trees and four different directions. Move by saying go north, go south, go west or go east. to escape from this creepy forest.";
+    const assets = {
+      mainImage: util.getS3PreSignedUrl("Media/forest2.svg")
+    };
+
+    renderDocumentForScreen(screenDocument, handlerInput, text, assets);
+
+    const speakOutput = text.main;
     return handlerInput.responseBuilder
       .speak(extraText + speakOutput)
       .reprompt(extraText + speakOutput)
@@ -269,11 +308,27 @@ const EastIntentIntentHandler = {
 
     console.log("EAST INTENT", "maze", maze, "player", player);
 
-    const { speakOutput, dieText, outBounds } = handleMovement("east");
+    const { speakOutput, dieText, outBounds, imageSrc } = handleMovement(
+      "east"
+    );
 
     if (outBounds) {
       return buildOutOfBoundsResponse(handlerInput, "east");
     }
+
+    const hideLife = dieText !== "" || imageSrc === "exit.svg";
+
+    const text = {
+      type: "object",
+      main: speakOutput,
+      life: hideLife ? "-1" : player.life + ""
+    };
+
+    const assets = {
+      mainImage: util.getS3PreSignedUrl(`Media/${imageSrc}`)
+    };
+
+    renderDocumentForScreen(screenDocument, handlerInput, text, assets);
 
     return handlerInput.responseBuilder
       .speak(speakOutput + dieText)
@@ -298,11 +353,27 @@ const WestIntentIntentHandler = {
       return buildNotStandUpResponse(handlerInput);
     }
 
-    const { speakOutput, dieText, outBounds } = handleMovement("west");
+    const { speakOutput, dieText, outBounds, imageSrc } = handleMovement(
+      "west"
+    );
 
     if (outBounds) {
       return buildOutOfBoundsResponse(handlerInput, "west");
     }
+
+    const hideLife = dieText !== "" || imageSrc === "exit.svg";
+
+    const text = {
+      type: "object",
+      main: speakOutput,
+      life: hideLife ? "-1" : player.life + ""
+    };
+
+    const assets = {
+      mainImage: util.getS3PreSignedUrl(`Media/${imageSrc}`)
+    };
+
+    renderDocumentForScreen(screenDocument, handlerInput, text, assets);
 
     return handlerInput.responseBuilder
       .speak(speakOutput + dieText)
@@ -327,11 +398,27 @@ const NorthIntentIntentHandler = {
       return buildNotStandUpResponse(handlerInput);
     }
 
-    const { speakOutput, dieText, outBounds } = handleMovement("north");
+    const { speakOutput, dieText, outBounds, imageSrc } = handleMovement(
+      "north"
+    );
 
     if (outBounds) {
       return buildOutOfBoundsResponse(handlerInput, "north");
     }
+
+    const hideLife = dieText !== "" || imageSrc === "exit.svg";
+
+    const text = {
+      type: "object",
+      main: speakOutput,
+      life: hideLife ? "-1" : player.life + ""
+    };
+
+    const assets = {
+      mainImage: util.getS3PreSignedUrl(`Media/${imageSrc}`)
+    };
+
+    renderDocumentForScreen(screenDocument, handlerInput, text, assets);
 
     return handlerInput.responseBuilder
       .speak(speakOutput + dieText)
@@ -356,11 +443,27 @@ const SouthIntentIntentHandler = {
       return buildNotStandUpResponse(handlerInput);
     }
 
-    const { speakOutput, dieText, outBounds } = handleMovement("south");
+    const { speakOutput, dieText, outBounds, imageSrc } = handleMovement(
+      "south"
+    );
 
     if (outBounds) {
       return buildOutOfBoundsResponse(handlerInput, "south");
     }
+
+    const hideLife = dieText !== "" || imageSrc === "exit.svg";
+
+    const text = {
+      type: "object",
+      main: speakOutput,
+      life: hideLife ? "-1" : player.life + ""
+    };
+
+    const assets = {
+      mainImage: util.getS3PreSignedUrl(`Media/${imageSrc}`)
+    };
+
+    renderDocumentForScreen(screenDocument, handlerInput, text, assets);
 
     return handlerInput.responseBuilder
       .speak(speakOutput + dieText)
